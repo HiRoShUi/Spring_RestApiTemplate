3929ec37163393e97fd8faf7d5c542886c8574ef
root <root@ip-172-31-21-13.eu-west-1.compute.internal>
2018-10-01 12:43:34 +0000

    changed script

--- org.makro.spring.restapi.general/ci/build.sh
+++ org.makro.spring.restapi.general/ci/build.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-cd source-code
-  ./mvnw clean package
+cd source-code/org.makro.spring.restapi.general
+	./mvnw clean package
 cd ..
 
 cp source-code/org.makro.spring.restapi.general/target/restapi.jar  build-output/.
