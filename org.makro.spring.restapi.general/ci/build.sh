5df5b25a9dccb056507e997016e0c923dd1f8684
root <root@ip-172-31-21-13.eu-west-1.compute.internal>
2018-10-01 12:55:12 +0000

    .

--- org.makro.spring.restapi.general/ci/build.sh
+++ org.makro.spring.restapi.general/ci/build.sh
@@ -5,5 +5,6 @@ set -e -x
 cd source-code/org.makro.spring.restapi.general
 	./mvnw clean package
 cd ..
+cd ..
 
 cp source-code/org.makro.spring.restapi.general/target/restapi-1.0.0.jar  build-output/.
